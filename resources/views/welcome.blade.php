8068f3c1d2a1c899fff80a20cafa7a308a410c30
raphaelsrodrigues <raph@raph-web.com>
2017-03-31 16:55:40 +0200

    Adding routes, views and controllers

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Andrzej Pawluzekinho: )))
+                    Main Page
                 </div>
 
                 <div class="links">
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,42 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+Route::get('/', 'CopiadoraController@index');
+Route::get('contact', 'CopiadoraController@contact');
+Route::get('address', 'CopiadoraController@address');
+Route::match(['get', 'post'], 'access', 'CopiadoraController@access');
+
+
+
+//Route::get('home', 'HomeController@index');
+//Route::controllers([
+//    'auth' => 'Auth\AuthController',
+//    'password' => 'Auth\PasswordController',
+//]);
+//// Authentication routes...
+//Route::get('auth/login', 'Auth\AuthController@getLogin');
+//Route::post('auth/login', 'Auth\AuthController@postLogin');
+//Route::get('auth/logout', 'Auth\AuthController@getLogout');
+//// Registration routes...
+//Route::get('auth/register', 'Auth\AuthController@getRegister');
+//Route::post('auth/register', 'Auth\AuthController@postRegister');
+//Route::get('hello', 'Hello@index');
+//Route::get('/hello/{name}', 'Hello@show');
+//Route::get('copiadora', 'CopiadoraController@index');
+//Route::match(['get', 'post'], 'status', 'CopiadoraController@status');
+//Route::match(['get', 'post'], 'inserirentrada', 'CopiadoraController@inserirentrada');
+//Route::match(['get', 'post'], 'inserirsaida', 'CopiadoraController@inserirsaida');
+//Route::match(['get', 'post'], 'administrador', 'CopiadoraController@administrador');
+//Route::match(['get', 'post'], '/administrador/check', 'CopiadoraController@viewday');
+//Route::match(['get', 'post'], '/adicionar', 'CopiadoraController@create');
+//Route::match(['get', 'post'], '/custos', 'CopiadoraController@custos');
+//Route::get('/excluir/{name}', 'CopiadoraController@destroy');
+//Route::get('/editar/{name}', 'CopiadoraController@edit');
+//Route::get('/mensagem', 'CopiadoraController@errors');
+//Route::get('/flush', 'CopiadoraController@sair');
+//Route::get('/allcosts', 'CopiadoraController@allcosts');
+//Route::get('/alldays', 'CopiadoraController@alldays');
