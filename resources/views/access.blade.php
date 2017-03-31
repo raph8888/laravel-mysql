3dc56b65237c86cc87a957122b10e295cf0e49be
raphaelsrodrigues <raph@raph-web.com>
2017-03-31 17:24:31 +0200

    refactor js

--- resources/views/access.blade.php
+++ resources/views/access.blade.php
@@ -11,9 +11,9 @@
 
             <h1>Acesso</h1>
 
-            <h1><small><div id="data"></div></small></h1><br>
+            <h1><small><div id="show_date"></div></small></h1><br>
 
-            <form action="http://copiadoramoc.com/public/acesso" method="post" enctype="multipart/form-data">
+            <form action={{ $path }}/acesso method="post" enctype="multipart/form-data">
 
                 <input type="text" name="user" placeholder="Nome" required>
                 <input type="password" name="pass" placeholder="Senha" required>
@@ -24,38 +24,19 @@
 
         </div>
 
-
         <script src='http://codepen.io/assets/libs/fullpage/jquery_and_jqueryui.js'></script>
 
     </div>
 
     <script>
 
-        NomeMes = new Array(12)
-        NomeMes[0] = "1"
-        NomeMes[1] = "2"
-        NomeMes[2] = "3"
-        NomeMes[3] = "4"
-        NomeMes[4] = "5"
-        NomeMes[5] = "6"
-        NomeMes[6] = "7"
-        NomeMes[7] = "8"
-        NomeMes[8] = "9"
-        NomeMes[9] = "10"
-        NomeMes[10] = "11"
-        NomeMes[11] = "12"
+        var date = new Date();
+        var day = date.getDate();
+        var month = date.getMonth() + 1;
+        var year = date.getFullYear();
 
-        var data=new Date();
-        var dia=data.getDate();
-        if(dia < 10) {
-            dia = "0" + dia;
-        }
-        var mes=data.getMonth();
-        var ano=data.getFullYear();
-
-        data = dia + ' / ' + NomeMes[mes] + ' / ' + ano;
-
-        document.getElementById("data").innerHTML = data;
+        date = day + ' / ' + month + ' / ' + year;
+        document.getElementById("show_date").innerHTML = date;
 
     </script>
 
