14fa1436759b5674e7bf03e75358c5323283ead1
raphaelsrodrigues <raph@raph-web.com>
2017-04-10 11:40:09 +0200

    UI change

--- resources/views/costs.blade.php
+++ resources/views/costs.blade.php
@@ -53,24 +53,21 @@
                 </tbody>
             </table>
 
-            <br>
-
-
-            <div class="include-cost-div" style="float:right; width: 33%; padding-top: 20px;">
+            <div class="include-cost-div" style="position: relative;
+    display: inline-block;
+    margin-top: 20px;
+    margin-bottom: 20px;">
                 <p>Incluir Despesa</p>
 
-                <input type="text" name="newcost" id="newcost" placeholder="Descrição" style="width: 98%;" required>
-                <br>
-                <br>
+                <input type="text" name="newcost" id="newcost" placeholder="Descrição" style="width: 40%;
+    display: inline;" required>
                 <input type="number" name="newvalue" id="newvalue" placeholder="Valor" step="0.01" min="0.01"
-                       max="99" style="width: 98%;" required/>
+                       max="99" style="width: 40%;
+    display: inline;" required/>
                 <br>
                 <br>
-                <input type="submit" name="add" class="add" id="add" value="Adicionar (+)" style="float:right;">
+                <input type="submit" name="add" class="add" id="add" value="Adicionar (+)">
             </div>
-
-            <br>
-            <br>
 
             {!! isset($resultado) ? $resultado : null !!}
 
