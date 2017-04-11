8f57cc2bbbc4fcbde566ff4136fb5d97478c79c5
raphaelsrodrigues <raph@raph-web.com>
2017-04-11 11:12:21 +0200

    Refactoring - more readable

--- resources/views/layouts/master2.blade.php
+++ resources/views/layouts/master2.blade.php
@@ -8,7 +8,6 @@
 
 </head>
 
-
 <?php $dayid = "li-" . date('d-m-Y'); ?>
 
 <style>
@@ -32,28 +31,41 @@
 
     <div class="row">
         <div class="col-xs-12">
-            <a href={{ url('/') }}><p
-                        style="float:right; padding-right: 20px; padding-top: 20px;">Sair da Área Administrativa</p>
+            <a href={{ url('/') }}>
+                <p style="float:right; padding-right: 20px; padding-top: 20px;">
+                    Sair da Área Administrativa
+                </p>
             </a>
         </div>
     </div>
 
 
     <div class="row">
+
         <div class="hidden-xs col-md-2">
+
             <img src="images/Untitled.png" style="width: 100%; max-width: 180px;">
-        </div>
-        <div class="col-xs-12 col-md-8 text-center">
-            <a href= {{ url('/') }} align="center"
-               style="text-decoration: none; color: #5196D5;">
-                <h1>Copiadora Montes Claros</h1>
-            </a>
-            <a href= {{ url('/status') }}><h2>Área Administrativa</h2></a>
 
         </div>
-        <div class="hidden-xs col-md-2">
-            <img src="images/Untitled.png" style="width: 100%; max-width: 180px;">
+
+        <div class="col-xs-12 col-md-8 text-center">
+
+            <a href={{ url('/') }} align="center" style="text-decoration: none; color: #5196D5;">
+                <h1>Copiadora Montes Claros</h1>
+            </a>
+
+            <a href= {{ url('/status') }}>
+                <h2>Área Administrativa</h2>
+            </a>
+
         </div>
+
+        <div class="hidden-xs col-md-2">
+
+            <img src="images/Untitled.png" style="width: 100%; max-width: 180px;">
+
+        </div>
+
     </div>
 
 </div>
