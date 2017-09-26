ec5f3e9ef520a94235a2d44b2ec2252008a38fd0
Raphael S. Rodrigues <raph@raph-web.com>
2017-09-26 21:34:30 +0200

    Update TextMessage
    Improves security by removing api_key (duh), adds properties and validation

--- app/Services/TextMessage/TextMessage.php
+++ app/Services/TextMessage/TextMessage.php
@@ -2,39 +2,60 @@
 
 namespace App\Services\TextMessage;
 
+use Entity\Config;
+use Exception\ConfigException;
+use Exception\ValidationException;
+
+use MessageBird\Client;
+use MessageBird\Objects\Message;
 
 class TextMessage
 {
 
-    const ACCESS_KEY = 'TvvSO0sf8MRQVstHzwoB6ybCx';
-    const COPIADORA = 'Copiadora Moc';
-
     public $client = '';
+    private $originator;
 
     public function __construct()
     {
-
-        $this->client = new \MessageBird\Client($this::ACCESS_KEY);
-
+        try {
+            $api_key = get_param("message_bird_api_key");
+        } catch (ConfigException $e) {
+            throw new RuntimeException('MessageBird API key can not be retrieved.');
+        }
+        
+        $this->client = new Client($api_key);
+        $this->originator = Config::read('organisation');
     }
 
-    public function sendsms($phones, $body) {
+    public function sendsms($phone_number = '', $message_text = '') {
+        
+        if (empty($message_text)) {
+            throw new ValidationException('Empty messages are invalid');
+        }
 
-        $Message = new \MessageBird\Objects\Message();
-        $Message->originator = 'MessageBird';
-        $Message->recipients = $phones;
-        $Message->body = $body;
+        if (strlen($phone_number) < 10) {
+            throw new ValidationException(sprintf('The number: %s is incorrect, it must have at'
+                . ' least 10 characters', $phone_number));
+        }
+        
+        if (get_param('dev_mode') === true) {
+            $phone_number = get_param('dev_sms_recipient');
+        } else {
+            $phone_number = $this->applyCountryCode($phone_number);
+        }
+        
+        $message = new Message;
+        $message->originator = $this->originator;
+        $message->recipients = [$phone_number];
+        $message->body = $message_text;
 
-        $message_status = $this->client->messages->create($Message);
+        $response = $this->client->messages->create($message);
 
-        return $message_status;
-
-
+        return $response;
     }
 
     public function getcredits() {
         $balance = $this->client->balance->read();
-
         return $balance;
     }
 
