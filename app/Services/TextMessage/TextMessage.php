e171f30a83df5e07a77db50b4e8ef6d715bef5fa
raphaelsrodrigues <raph@raph-web.com>
2017-11-05 14:29:35 +0100

    reverting changes

--- app/Services/TextMessage/TextMessage.php
+++ app/Services/TextMessage/TextMessage.php
@@ -2,76 +2,40 @@
 
 namespace App\Services\TextMessage;
 
-use Entity\Config;
-use Exception\ConfigException;
-use Exception\ValidationException;
-
-use MessageBird\Client;
-use MessageBird\Objects\Message;
 
 class TextMessage
 {
 
+    const ACCESS_KEY = 'TvvSO0sf8MRQVstHzwoB6ybCx';
+    const COPIADORA = 'Copiadora Moc';
+
     public $client = '';
-    private $originator;
 
     public function __construct()
     {
-        try {
-            $api_key = get_param("message_bird_api_key");
-        } catch (ConfigException $e) {
-            throw new RuntimeException('MessageBird API key can not be retrieved.');
-        }
-        
-        $this->client = new Client($api_key);
-        $this->originator = Config::read('organisation');
+
+        $this->client = new \MessageBird\Client($this::ACCESS_KEY);
+
     }
 
-    public function sendsms($phone_number = '', $message_text = '')
-    {
-        if (empty($message_text)) {
-            throw new ValidationException('Empty messages are invalid');
-        }
+    public function sendsms($phones, $body) {
 
-        if (strlen($phone_number) < 10) {
-            throw new ValidationException(sprintf('The number: %s is incorrect, it must have at'
-                . ' least 10 characters', $phone_number));
-        }
-        
-        if (get_param('dev_mode') === true) {
-            $phone_number = get_param('dev_sms_recipient');
-        } else {
-            $phone_number = $this->applyCountryCode($phone_number);
-        }
-        
-        $message = new Message;
-        $message->originator = $this->originator;
-        $message->recipients = [$phone_number];
-        $message->body = $message_text;
+        $Message = new \MessageBird\Objects\Message();
+        $Message->originator = 'MessageBird';
+        $Message->recipients = $phones;
+        $Message->body = $body;
 
-        $response = $this->client->messages->create($message);
+        $message_status = $this->client->messages->create($Message);
 
-        return $response;
+        return $message_status;
+
+
     }
 
-    public function getcredits()
-    {
+    public function getcredits() {
         $balance = $this->client->balance->read();
+
         return $balance;
-    }
-    
-    public function applyCountryCode($phone_number)
-    {
-        if (substr($phone_number, 0, 4) == '0031') {
-            return '31' . substr($phone_number, 4);
-        } elseif (substr($phone_number, 0, 3) == '316') {
-            return $phone_number;
-        } elseif (substr($phone_number, 0, 2) == '06') {
-            return substr_replace($phone_number,'31', 0, 1);
-        }
-        
-        throw new ValidationException(sprintf('Unexpected format of phone number %s'
-            .', expected it starts with 0031, 31 or 316', $phone_number));
     }
 
 }
