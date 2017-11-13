5fabb606603f76b512b296d4499d5e8e513f93b0
raphaelsrodrigues <raph@raph-web.com>
2017-11-13 20:53:45 +0100

    remove spaces

--- resources/views/inserirsaida.blade.php
+++ resources/views/inserirsaida.blade.php
@@ -2,12 +2,8 @@
 @section('content')
 
     <br>
-    <br>
-
     <p style="color: red;">{!! isset($error) ? $error : null !!}</p>
 
-    <br>
-    <br>
     <div class="login-card">
         <div id="data"></div>
         <h1>Fechamento do Caixa</h1><br>
